c723dcc078c3f6b895231b5f5fbc3e40e712f555
csnight <923739637@qq.com>
2020-04-01 22:35:37 +0800

    add examples

--- examples/js/config.js
+++ examples/js/config.js
@@ -1,6 +1,74 @@
 let examples = [
   {
-    name: '地图控件',
-    path: './pages/controls.html'
+    name: '加载地图',
+    path: './pages/createMap.html'
+  },
+  {
+    name: '地图工具',
+    path: './pages/mapTools.html'
+  },
+  {
+    name: '绘制工具',
+    path: './pages/mapDraw.html'
+  },
+  {
+    name: '地图格网',
+    path: './pages/mapGrid.html'
+  },
+  {
+    name: '地图切换控件',
+    path: './pages/mapSwitch.html'
+  },
+  {
+    name: '图层目录树',
+    path: './pages/layerTree.html'
+  },
+  {
+    name: '瓦片地图API',
+    path: './pages/tileLayers.html'
+  },
+  {
+    name: '客户端分析工具',
+    path: './pages/clientDistance.html'
+  },
+  {
+    name: '客户端缓冲分析',
+    path: './pages/clientBuffer.html'
+  },
+  {
+    name: '客户端缓冲查询',
+    path: './pages/clientBufferSearch.html'
+  },
+  {
+    name: '客户端多边形查询',
+    path: './pages/clientGeoSearch.html'
+  },
+  {
+    name: '客户端相交分析',
+    path: './pages/clientIntersect.html'
+  },
+  {
+    name: '客户端异或分析',
+    path: './pages/clientDifference.html'
+  },
+  {
+    name: '客户端多边形融合',
+    path: './pages/clientUnion.html'
+  },
+  {
+    name: '客户端计算凹凸包',
+    path: './pages/clientEnvelope.html'
+  },
+  {
+    name: '客户端TIN三角网',
+    path: './pages/clientTIN.html'
+  },
+  {
+    name: '客户端生成等值线',
+    path: './pages/clientIsoLine.html'
+  },
+  {
+    name: '客户端插值分析',
+    path: './pages/clientIsoIntersect.html'
   }
 ];

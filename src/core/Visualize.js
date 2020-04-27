56e544e6891cf6ca8cd7598a22ebfd557359c688
chensi <chensi@thtf.com.cn>
2020-04-27 11:19:58 +0800

    add echart popup layer

--- src/core/Visualize.js
+++ src/core/Visualize.js
@@ -3,13 +3,15 @@ import {FlowField} from "../visualize/FlowField";
 import PointCluster from "../visualize/PointCluster";
 import ClassicRender from "../visualize/ClassicRender";
 import ChartRender from "../visualize/ChartRender";
+import CanvasHPLayer from "../visualize/CanvasHPLayer";
 
 export default {
   ...HeatMap,
   FlowField,
   ...PointCluster,
   ...ClassicRender,
-  ...ChartRender
+  ...ChartRender,
+  ...CanvasHPLayer
 }
 
 
